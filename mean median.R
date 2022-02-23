c9152cc5b4805e40c164a820456bdddcbe49faa2
lekhbisht <lekhanibisht02@gmail.com>
2022-02-23 13:48:52 +0530

    my mean median mode commit.

---
+++ mean median.R
@@ -0,0 +1,15 @@
+function mean()
+
+P= c(4,5,6,7,8,1,2,3,4)
+result.mean = mean(P)
+print(result.mean)
+
+
+
+
+function median()
+
+
+x= c(10,11,12,13,14,15,16,17,19)
+median.result = median(x)
+print(median.result)
